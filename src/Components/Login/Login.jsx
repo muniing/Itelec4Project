50bcebdebb8b8f1161f377087aeb74c90326526e
Michelle <placenciamitel14@gmail.com>
2023-12-14 19:46:21 +0800

    login and signup

---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react"
+import './Login.css'
+import user_icon from '../Assests/person.png'
+import email_icon from '../Assests/email.png'
+import password_icon from '../Assests/password.png'
+
+const Login = () => {
+
+
+    return (
+        <div className="container">
+            <div className="header">
+                <div className="text">Login</div>
+                <div className="underline"></div>
+            </div>
+            <div className="inputs">
+                <div className="input">
+                    <img src={email_icon} alt=""/>
+                    <input type="email" placeholder="Email"/>
+                </div>
+                <div className="input">
+                    <img src={password_icon} alt=""/>
+                    <input type="password" placeholder="Password"/>
+                </div>
+                <div className="create_account">Create Account? <span>Click here!</span></div>
+                
+                <div className="submit_container">
+                    <div className="submit">Login</div>
+                </div>
+            </div>
+        </div>
+    
+        
+    )
+}
+
+export default Login;
---
+++ src/Components/Signup/Signup.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react"
+import './Signup.css'
+import user_icon from '../Assests/person.png'
+import email_icon from '../Assests/email.png'
+import password_icon from '../Assests/password.png'
+
+const Signup = () => {
+
+    return (
+        <div className="container">
+            <div className="header">
+                <div className="text">Sign Up</div>
+                <div className="underline"></div>
+            </div>
+            <div className="inputs">
+            <div className="input">
+                    <img src={user_icon} alt=""/>
+                    <input type="text" placeholder="Name"/>
+                </div>
+                <div className="input">
+                    <img src={email_icon} alt=""/>
+                    <input type="email" placeholder="Email"/>
+                </div>
+                <div className="input">
+                    <img src={password_icon} alt=""/>
+                    <input type="password" placeholder="Password"/>
+                </div>
+                <div className="existing_account">Already have an account? <span>Click here!</span></div>
+                <div className="submit_container">
+                    <div className="submit">Sign Up</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Signup;
